3854f6a630a66980f9730bfead2d971fcf98203a
BOSS <bigboss1796@hotmail.com>
2023-12-06 15:36:33 +0700

    feat:Callbacks and Higher Order Functions

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,21 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let morethan70 = [];
+  for (let i=0; i<array.length; i++){
+    operation(array[i],morethan70);
+  }
+  if (morethan70.length >= 5) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function checkFive(number,newpara){
+  if (number > 70){
+    return newpara.push(number)
+  }
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +25,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1,checkFive);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2,checkFive);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3,checkFive);
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
