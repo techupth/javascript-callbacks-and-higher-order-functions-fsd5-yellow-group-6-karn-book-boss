c5bf138af93ec0885928cb578fdfa9fce523e673
Supakit <book0970403815@hotmail.com>
2023-12-06 15:15:48 +0700

    feat : send assignment by book

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,17 @@
 // Exercise #1: For Each Function
 
 function forEach(array, operation) {
-  // Start coding here
+  for (let i = 0; i < array.length; i++) {
+    newEmployeeSalaries.push(operation(array[i]));
+  }
 }
-
+function operation(array) {
+  array = array + 5000;
+  return array;
+}
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
-// Using `forEach` function here
+forEach(employeeSalaries, operation);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,26 @@
-//Exercise #2: At Least Five Function
-
 function atLeastFive(array, operation) {
-  // Start coding here
+  add = [];
+  for (let i = 0; i < array.length; i++) {
+    operation(array[i]);
+  }
+  return add.length >= 5;
 }
+
+let add = [];
+let checkScores = function (array) {
+  if (array >= 70) {
+    add.push(array);
+  }
+};
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-// Using `atLeastFive` function here
-
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, checkScores);
+console.log(add);
+console.log(scoreRoom1Result);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, checkScores);
+console.log(scoreRoom2Result);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, checkScores);
+console.log(scoreRoom3Result);
